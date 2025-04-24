5772e209a7962a31ab019b610b94d1b13832eeed
sumamakhan7611 <khansumama87111@gmail.com>
2025-04-24 18:13:13 +0530

    update

---
+++ starter/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello world")
+}
+
+// go build starter/main.go now you see main.exe file for run the exe file use ./main -> why main bcz of our go file name is main -> now you see Hello World in the terminal
+
+// other method to do go run starter/main.go -> now see in the terminal -> Hello World
