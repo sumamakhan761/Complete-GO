a72e20789acf3403217b458ed53dbbc103a5b6d3
sumamakhan7611 <khansumama87111@gmail.com>
2025-04-24 20:18:46 +0530

    variable

--- variable/variable-1.go
+++ variable/variable-1.go
@@ -3,10 +3,43 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println(1);
-	fmt.Println(1+1);
-	fmt.Println("1" + "1");
-	fmt.Println(1*1);// mul
-	fmt.Println(10.5);// floars
-	fmt.Println(true);// boolean
+	// fmt.Println(1);
+	// fmt.Println(1+1);
+	// fmt.Println("1" + "1");
+	// fmt.Println(1*1);// mul
+	// fmt.Println(10.5);// floars
+	// fmt.Println(true);// boolean
+
+	// variable 
+	// var name = "sumama"
+	// var secondname string = "sumama"
+	// fmt.Println(name)
+	// fmt.Println(secondname)
+
+	// var isAdult = true
+	// fmt.Println(isAdult)
+
+	// shorthand syntax
+	// name:="sumama"
+	// fmt.Println(name)
+
+	// when you not declate so you need to declare
+
+	// var name string
+	// name = "golang"
+
+	// fmt.Println(name)
+
+
+	// constant
+	// const c = 2;
+	// // c = 4 // we can't assign to value bcz contant value is contant 
+	// fmt.Println(c)
+
+	const(
+		host = "localhost"
+		port = 5000
+	)
+
+	fmt.Println(host,port)
 }
