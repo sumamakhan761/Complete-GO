d4cc62af4b858fa0da12034cf8d7ddf895ea5f65
sumamakhan7611 <khansumama87111@gmail.com>
2025-05-12 23:04:03 +0530

    update

---
+++ 20_mutex/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+// mutex uses to handle a rase problem
+type post struct {
+	views int
+	mu sync.Mutex
+}
+
+func (p *post) inc(wg *sync.WaitGroup) {
+	defer func(){
+		wg.Done()
+		p.mu.Unlock()
+	}()
+	p.mu.Lock()
+	p.views += 1
+}
+
+func main() {
+	var wg sync.WaitGroup
+	mypost := post{views: 0}
+	for i:=0; i < 100; i++{
+		wg.Add(1)
+	go mypost.inc(&wg)
+}	
+// lots of time request come concrently 
+	wg.Wait()
+	fmt.Println(mypost.views)
+}
