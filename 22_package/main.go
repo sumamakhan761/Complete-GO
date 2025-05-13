4e6185e2bf8d60d6730ed19f0c5b940dc5c10387
sumamakhan7611 <khansumama87111@gmail.com>
2025-05-13 21:40:50 +0530

    update

---
+++ 22_package/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/sumamakhan761/Complete-GO/auth"
+	"github.com/sumamakhan761/Complete-GO/user"
+	"github.com/fatih/color"
+)
+
+func main() {
+	auth.LoginWithCredentials("codersgyan", "secret")
+	session := auth.GetSession()
+
+	fmt.Println("session", session)
+
+	user := user.User{
+		Email: "user@email.com",
+		Name:  "John Doe",
+	}
+
+	// fmt.Println(user.Email, user.Name)
+	color.Green(user.Email)
+	color.Green(user.Name)
+
+}
